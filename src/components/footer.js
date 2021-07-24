0f1b69aabcfecb309319dd7c582dc3b0ee118b79
Peter Bishop <pjb.den@gmail.com>
2021-07-24 09:44:23 -0600

    header style applied

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,10 @@ const Footer = () => {
 
   return (
     <footer className="w-100 mt-auto text-dark p-4">
-      <div className="container text-center mb-5">
+      <div
+        className="container text-center mb-5"
+        //style={{ backgroundColor: "red" }}
+      >
         {location.pathname !== "/" && (
           <button
             className="btn btn-dark mb-3"
--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import BackgroundImage from "../assets/codeimage.jpg";
 
 const Header = () => {
+  const style = {
+    Link: {
+      fontFamily: "'Ubuntu Mono', monospace",
+      color: "white",
+      padding: "15px",
+    },
+    navDiv: {
+      textAlign: "right",
+    },
+    headerDiv: {
+      backgroundImage: `url(${BackgroundImage})`,
+      backgroundSize: "cover",
+    },
+    h1: {
+      fontSize: "3rem",
+      color: "white",
+      fontFamily: "'Libre Barcode 39 Text', cursive",
+    },
+  };
+
   return (
-    <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
-        <Link className="text-dark" to="/">
-          <h1 className="m-0" style={{ fontSize: "3rem" }}>
+    <div style={style.headerDiv}>
+      <header className="bg-info text-light mb-4 py-3 display-flex align-center">
+        <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
+          <h1 className="m-0" style={style.h1}>
             Peter J Bishop
           </h1>
-        </Link>
-        <p className="m-0" style={{ fontSize: "1.75rem", fontWeight: "700" }}>
-          Peter Bishop's Portfolio
-        </p>
-        <div>
-          <>
-            <Link className="btn btn-lg btn-light m-2" to="/home">
-              Home
-            </Link>
-            <Link className="btn btn-lg btn-light m-2" to="/about">
-              About
-            </Link>
-            <Link className="btn btn-lg btn-light m-2" to="/Work">
-              Work{" "}
-            </Link>
-            <Link className="btn btn-lg btn-light m-2" to="/contact">
-              Contact
-            </Link>
-          </>
+
+          <div style={style.navDiv}>
+            <>
+              <Link
+                variant="outline-dark"
+                className="btn btn-lg m-2"
+                to="/home"
+                style={style.Link}
+              >
+                HOME
+              </Link>
+              <Link
+                variant="outline-dark"
+                className="btn btn-lg m-2"
+                to="/about"
+                style={style.Link}
+              >
+                ABOUT
+              </Link>
+              <Link className="btn btn-lg m-2" to="/Work" style={style.Link}>
+                WORK
+              </Link>
+              <Link className="btn btn-lg m-2" to="/contact" style={style.Link}>
+                CONTACT
+              </Link>
+            </>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
 
