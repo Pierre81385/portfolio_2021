afd2fa2a1a273dd39ac7542f23ee941f499d3d4f
Peter Bishop <pjb.den@gmail.com>
2021-08-04 11:29:45 -0600

    removed header footer for cleaner look, added ABOUT content, styled contact links

---
+++ src/utils/backgroundVideo.js
@@ -0,0 +1,31 @@
+import { classes } from "istanbul-lib-coverage";
+import React from "react";
+import myVideo from "../assets/Black - 13495.mp4";
+import { Link } from "react-router-dom";
+
+const BackgroundVideo = () => {
+  const style = {
+    video: {
+      position: "fixed",
+      right: 0,
+      bottom: 0,
+      minWidth: "100%",
+      minHeight: "100%",
+    },
+  };
+  return (
+    <div className={classes.Container}>
+      <video
+        autoPlay="autoplay"
+        loop="loop"
+        muted
+        className={classes.Video}
+        style={style.video}
+      >
+        <source src={`${myVideo}`} type="video/mp4" />
+      </video>
+    </div>
+  );
+};
+
+export default BackgroundVideo;
